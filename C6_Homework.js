d9bede7e966293e164744c3bd9716f0dcae32844
Dimasik19 <119976425+Dimasik19@users.noreply.github.com>
2023-05-12 16:26:42 +0300

    Update C6_Homework.js

--- C6_Homework.js
+++ C6_Homework.js
@@ -1,1 +1,42 @@
+const output = document.getElementById("output");
+const value = document.querySelector('input').value;
 
+const btnOpen = document.querySelector('.j-btn-geo');
+const btnSend = document.querySelector('.j-btn-send');
+
+btnSend.addEventListener('click', () => {
+  const message = 'Test message';
+  writeToScreen("SENT: " + message);
+  websocket.send(message);
+});
+const status = document.querySelector('#status');
+const mapLink = document.querySelector('#map-link');
+const btn = document.querySelector('.j-btn-test');
+
+// Функция, выводящая текст об ошибке
+const error = () => {
+  status.textContent = 'Невозможно получить ваше местоположение';
+}
+
+// Функция, срабатывающая при успешном получении геолокации
+const success = (position) => {
+  console.log('position', position);
+  const latitude  = position.coords.latitude;
+  const longitude = position.coords.longitude;
+
+  status.textContent = `Широта: ${latitude} °, Долгота: ${longitude} °`;
+  mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+  mapLink.textContent = 'Ссылка на карту';
+}
+
+btn.addEventListener('click', () => {
+  mapLink.href = '';
+  mapLink.textContent = '';
+  
+  if (!navigator.geolocation) {
+    status.textContent = 'Geolocation не поддерживается вашим браузером';
+  } else {
+    status.textContent = 'Определение местоположения…';
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+});
